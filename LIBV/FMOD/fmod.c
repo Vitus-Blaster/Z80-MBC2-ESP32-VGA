b72b28704fc64e2de1ebace5ceda458f155dc5e9
Vito Blasi <68391189+Vitus-Blaster@users.noreply.github.com>
2020-12-19 11:25:03 +0100

    Update fmod.c

--- LIBV/FMOD/fmod.c
+++ LIBV/FMOD/fmod.c
@@ -7,41 +7,41 @@ double x, y;
 {
 	double mode;
 
-	if (y = = 0.0)             /* if the divisor is zero          */
+	if (y = = 0.0)                 /* if the divisor is zero          */
 	{
 		return Infinity;       /* see float.h                     */
 	}
 
-	if (x = = 0.0)             /* if the dividend is zero         */
+	if (x = = 0.0)                 /* if the dividend is zero         */
 	{
 		return 0.0;            /* return zero                     */
 	}
 
-	if (x < 0.0)               /* if the dividend is negative     */
+	if (x < 0.0)                   /* if the dividend is negative     */
 	{
 		                       /* the initial value of the        */
 		mod = - x;             /* mod is equal to dividend        */
-	}                          /* but positive                    */
-	                           /* made positive                   */
-	else                       /* otherwise                       */
-	{                          /* the initial value of the module */
+	}                              /* but positive                    */
+	                               /* made positive                   */
+	else                           /* otherwise                       */
+	{                              /* the initial value of the module */
 		mod = x;               /* is equal to the dividend        */
 	}
 
-	if (y < 0.0)               /* if the divisor is negative      */
+	if (y < 0.0)                   /* if the divisor is negative      */
 	{
 		y = -y;	               /* becomes positive                */
 	}
 
-	while (mod > = y)          /* subtraction cycle of            */
-	{                          /* divisor from dividend,          */
-		mod = mod - y;         /* in output we will have a value  */
-	}                          /* lower than dividend             */
+	while (mod > = y)             /* subtraction cycle of            */
+	{                             /* divisor from dividend,          */
+		mod = mod - y;        /* in output we will have a value  */
+	}                             /* lower than dividend             */
 
-	if (x < 0.0)               /* if the dividend was negative    */
+	if (x < 0.0)                  /* if the dividend was negative    */
 	{
-		return -mod;           /* the module is negative          */
+		return -mod;          /* the module is negative          */
 	}
 
-	return mod;               /* returns the module              */
+	return mod;                   /* returns the module              */
 }
