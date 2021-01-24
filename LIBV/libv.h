436bbde28ccf95042eab44361c03867aa712b0dc
Vito Blasi <68391189+Vitus-Blaster@users.noreply.github.com>
2021-01-24 11:03:22 +0100

    Delete libv.h

--- LIBV/libv.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HTC_LIBV_H
-#define _HTC_LIBV_H
-
-extern double fmod(double, double), radcub(double, double);
-extern double power(double, double), logn(int, double);
-extern char* itoa(int, char*, int);
-extern double segno(double);
-#endif
