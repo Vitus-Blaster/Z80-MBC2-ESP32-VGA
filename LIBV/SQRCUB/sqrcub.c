af8c620f87826f6e0bc8cd769e8d4788323853df
Vito Blasi <68391189+Vitus-Blaster@users.noreply.github.com>
2021-01-24 15:54:40 +0100

    Add files via upload

---
+++ LIBV/SQRCUB/sqrcub.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+extern double pow(double,double);
+
+double radcub(r)
+double r;
+{
+        double y;
+	y = pow(r, 0.3333333);
+	return y;
+}
+
+
+
+
