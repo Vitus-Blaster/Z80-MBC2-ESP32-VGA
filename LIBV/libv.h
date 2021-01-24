b60ca79fb98fb9226dbabc6a9d4b559c02847bc2
Vito Blasi <68391189+Vitus-Blaster@users.noreply.github.com>
2021-01-24 15:53:37 +0100

    Add files via upload

--- LIBV/libv.h
+++ LIBV/libv.h
@@ -1,11 +1,13 @@
 #ifndef _HTC_LIBV_H
 #define _HTC_LIBV_H
 
+extern double exp(double);
 extern double floor(double);
 extern double fmod(double, double);
 extern char* itoa(int, char*, int);
 extern double ldexp(double, int);
 extern double logn(int, double);
-extern double sqrcub(double, double);
 extern double power(double, double);
+extern double sqrcub(double, double);
+
 #endif
