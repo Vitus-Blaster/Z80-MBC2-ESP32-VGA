d47552431e600ce3b7b1fb24405ec9a7a5273699
Vito Blasi <68391189+Vitus-Blaster@users.noreply.github.com>
2021-01-24 15:46:08 +0100

    Add files via upload

--- LIBV/libv.h
+++ LIBV/libv.h
@@ -4,4 +4,5 @@
 extern double fmod(double, double), radcub(double, double);
 extern double power(double, double), logn(int, double);
 extern char* itoa(int, char*, int);
-#endif
+extern double floor(double);
+#endif
